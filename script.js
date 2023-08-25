c71dc7a3785838e4093710ef6c9b2ef99380badc
Siddharth Kosaraju <siddhukosaraju@gmail.com>
2023-08-25 21:30:58 +0530

    Added logic to edit text of display

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
-function add(num1, num2) {
-    return num1 + num2;
+display = document.querySelector('#display');
+display.textContent = "0";
+
+zero = document.querySelector('#zero');
+one = document.querySelector('#one'); 
+two = document.querySelector('#two');
+three = document.querySelector('#three');
+four = document.querySelector("#four");
+five = document.querySelector('#five');
+six = document.querySelector('#six');
+seven = document.querySelector('#seven');
+eight = document.querySelector('#eight');
+nine = document.querySelector('#nine'); 
+numbers = document.querySelectorAll('.number');
+numberOrder = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function() {
+        display.textContent = display.textContent + numberOrder[i];
+    });
 }
 
-function subtract(num1, num2) {
-    return num1 - num2;
+left = document.querySelector('#left');
+right = document.querySelector('#right');
+left.addEventListener('click', function() {
+    display.textContent = display.textContent + '(';
+});
+right.addEventListener('click', function() {
+    display.textContent = display.textContent + ')';
+});
+
+decimal = document.querySelector('#decimal');
+decimal.addEventListener('click', function() {
+    display.textContent = display.textContent + '.';
+});
+
+add = document.querySelector('#add');
+subtract = document.querySelector('#subtract');
+multiply = document.querySelector('#multiply');
+divide = document.querySelector('#divide');
+operators = document.querySelectorAll('.operation');
+operatorOrder = ['/', '*', '-', '+'];
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', function() {
+        display.textContent = display.textContent + operatorOrder[i]; 
+    });
 }
 
-function multiply(num1, num2) {
-    return num1 * num2;
-}
+clr = document.querySelector('#clr');
+del = document.querySelector('#del');
+clr.addEventListener('click', function() {
+    display.textContent = '';
+});
+del.addEventListener('click', function() {
+    display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+});
 
-function divide(num1, num2) {
-    return num1 / num2;
-}
 
-let num1;
-let num2;
-let operator; 
+equal = document.querySelector('#evaluate');
+equal.addEventListener('click', function() {
+    
+});
